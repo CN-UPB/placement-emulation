fee9858d294ce1bed76ca0c9f3c00e8c8ae93cbf
Stefan Schneider <st.b.s@web.de>
2017-12-19 10:40:07 +0100

    added rest calls to create vnfs

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import csv
+import requests
 
 
 # parse placement result and issue REST API calls to instantiate and chain the VNFs in the emulator accordingly
 def emulate_embedding(embedding):
+	compute_url = "http://127.0.0.1:5001/restapi/compute/"
+	network_url = "http://127.0.0.1:5001/restapi/network/"
 	read_instances, read_edges = False, False
 	with open(embedding, "r") as embedding_file:
 		reader = csv.reader((row for row in embedding_file), delimiter="\t")
@@ -19,8 +22,10 @@ def emulate_embedding(embedding):
 				# recreate instances from previous embedding
 				if read_instances:
 					if len(row) == 2:
-						pass
-						# TODO: rest call to create vnf
+						dc = row[1];
+						vnf = row[0];
+						response = requests.put(compute_url + f"{dc}/{vnf}")
+						print(response)
 
 				# recreate edges from previous embedding
 				if read_edges:
