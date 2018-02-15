753a815c5ecc970063393054283b31e0a76e9dc6
Stefan Schneider <stefan.schneider@upb.de>
2018-02-15 14:46:55 +0100

    deploy and measure for 1 L4FW

---
+++ measure_1fw.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Latency between VNFs (ping)"
+echo "User -> L4FW 1"
+sudo docker exec -it mn.vnf_user ping -c5 -q 88.0.0.2
+echo "\nL4FW 1 -> Web"
+sudo docker exec -it mn.vnf_fw1 ping -c5 -q 99.0.0.2
+
+echo "\nLatency of whole chain"
+sudo docker exec -it mn.vnf_user httping --url http://88.0.0.2 -p 80 -c 5
