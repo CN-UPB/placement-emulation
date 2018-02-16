19719ee1df0eca3568558d44ab465303979d31e1
Stefan Schneider <stefan.schneider@upb.de>
2018-02-16 10:42:30 +0100

    moved measure scripts into separate folder

--- measurement/measure_1fw.sh
+++ measurement/measure_1fw.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 echo "Latency between VNFs (ping)"
+# why is this ping so high (~100ms) on the same pop?
 echo "User -> L4FW 1"
 sudo docker exec -it mn.vnf_user ping -c5 -q 88.0.0.2
 echo "\nL4FW 1 -> Web"
 sudo docker exec -it mn.vnf_fw1 ping -c5 -q 99.0.0.2
 
+# FIXME: why does the connection time out?
 echo "\nLatency of whole chain"
 sudo docker exec -it mn.vnf_user httping --url http://88.0.0.2 -p 80 -c 5
