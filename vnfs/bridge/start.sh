f4a918d0e788e14fa8c0938ce95de921f689c38d
Stefan Schneider <stefan.schneider@upb.de>
2018-02-16 10:40:08 +0100

    added bridge VNF

---
+++ vnfs/bridge/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# remove IPs from input/output interface to prepare them for bridging
+ip addr flush dev $IFIN
+ip addr flush dev $IFOUT
+
+# bridge interfaces (layer 2) and let snort listen to bridge (IDS mode)
+brctl addbr br0
+brctl addif br0 $IFIN $IFOUT
+ifconfig br0 up
+
--- vnfs/build.sh
+++ vnfs/build.sh
@@ -7,3 +7,4 @@ docker build -t placement-squid-img -f squid/Dockerfile squid
 docker build -t placement-user-img -f user/Dockerfile user
 docker build -t placement-fw1-img -f fw1/Dockerfile fw1
 docker build -t placement-fw2-img -f fw2/Dockerfile fw2
+docker build -t placement-bridge-img -f bridge/Dockerfile bridge
