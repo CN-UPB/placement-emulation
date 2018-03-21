a0ef91f780dda05b5aa919668922b0ddfdf27af3
Stefan Schneider <stefan.schneider@upb.de>
2018-03-21 11:30:18 +0100

    included conversion of measurement logs to yaml in the pipeline

--- test.sh
+++ test.sh
@@ -46,6 +46,10 @@ echo "network: $NETWORK" >> $LOG
 echo "service: $SERVICE" >> $LOG
 echo "sources: $SOURCES" >> $LOG
 
+# convert log to structured yaml file
+python3 util/log_parser.py -f $LOG
+rm $LOG
+
 # stop: find the pids and stop the process (will automatically clean up)
 pgrep -f "python emu" | sudo xargs kill
 sleep 5
