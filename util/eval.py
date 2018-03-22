d8f422bb0947a0efe343eaefbd22eb39bc8233c7
Stefan Schneider <stefan.schneider@upb.de>
2018-03-22 22:13:30 +0100

    match measurement results before comparing them => much less chaotic results...

--- util/eval.py
+++ util/eval.py
@@ -7,60 +7,69 @@ import matplotlib.pyplot as plt
 # TODO: store and process delays in an ordered fashion! (see fixme below)
 
 def process_emu_results():
-    # TODO: clean up/remove separate lists for delay, std, etc
     emu_results = glob.glob('../eval/emulation/*.yaml')
     emu_delays = []
-    emu_chain_rtt = []
-    emu_vnf_rtt = []
-    emu_vnf_std = []
     for result_file in emu_results:
         with open(result_file, 'r') as f:
             result = yaml.load(f)
 
             # chain delays = httping = TCP handshake = 2x RTT
             result['chain_rtt'] = result['chain_delay']['delay'] / 2
-            emu_chain_rtt.append(result['chain_delay']['delay'] / 2)
 
             # vnf delays = ping = RTT
             for delay in result['delays']:
                 delay['rtt'] = delay['delay']
-                emu_vnf_rtt.append(delay['delay'])
-                emu_vnf_std.append(delay['stddev'])
 
             emu_delays.append(result)
     return emu_delays
 
 
-def process_bjointsp_results():
-    # TODO: calc rtts and return process result list similar to emu results
-    bjointsp_results = glob.glob('../eval/bjointsp/*.yaml')
-    bjointsp_delays = []
-    bjointsp_chain_length = []      # num vnfs in the chain
-    bjointsp_chain_rtt = []
-    bjointsp_vnf_length = []        # num vnfs in the chain
-    bjointsp_vnf_rtt = []
-    for result_file in bjointsp_results:
+def process_sim_results():
+    sim_results = glob.glob('../eval/bjointsp/*.yaml')
+    sim_delays = []
+    for result_file in sim_results:
         with open(result_file, 'r') as f:
             result = yaml.load(f)
-            bjointsp_delays.append(result)
-            # cut chain length from service name, eg, "fw1chain.yaml" - WARNING: this will not work with generic names
-            chain_length = int(result['input']['service'][2]) + 2        # +2 for user and web
-            bjointsp_chain_length.append(chain_length)
+
+            # TODO: extract chain length, num flows...? or better write to file
+            # chain_length = int(result['input']['service'][2]) + 2        # +2 for user and web
+
             # one-way = RTT/2
-            bjointsp_chain_rtt.append(result['metrics']['total_delay'] * 2)
+            result['chain_rtt'] = result['metrics']['total_delay'] * 2
             for delay in result['metrics']['delays']:
-                bjointsp_vnf_length.append(chain_length)
-                bjointsp_vnf_rtt.append(delay['delay'] * 2)
+                delay['rtt'] = delay['delay'] * 2
+
+            sim_delays.append(result)
+    return sim_delays
 
 
-def plot():
-    # TODO: work with sorted, processed result lists instead of separate lists for everything
-    # plot chain delays
+def plot(emu_delays, sim_delays):
+    # TODO: very hacky and inefficient. what's a more elegant way?
+    # match and sort chain and inter-VNF RTTs
+    emu_chain_rtt, sim_chain_rtt = [], []
+    emu_vnf_rtt, sim_vnf_rtt = [], []
+    for emu in emu_delays:
+        for sim in sim_delays:
+            if emu['input']['network'].endswith(sim['input']['network']) \
+                    and emu['input']['service'].endswith(sim['input']['service']) \
+                    and emu['input']['sources'].endswith(sim['input']['sources']):
+                emu_chain_rtt.append(emu['chain_rtt'])
+                sim_chain_rtt.append(sim['chain_rtt'])
+
+                # match inter-VNF RTTs
+                for emu_vnf in emu['delays']:
+                    for sim_vnf in sim['metrics']['delays']:
+                        if emu_vnf['src'] == sim_vnf['src'] and emu_vnf['dest'] == sim_vnf['dest']:
+                            emu_vnf_rtt.append(emu_vnf['rtt'])
+                            sim_vnf_rtt.append(sim_vnf['rtt'])
+
+
+    # absolute chain delays
     plt.figure(1)
-    x_chain = range(len(emu_chain_rtt))
+    x_chain = range(len(emu_delays))
     plt.subplot(2, 1, 1)
-    plt.plot(x_chain, emu_chain_rtt, 'g^', x_chain, bjointsp_chain_rtt, 'bs')
-    plt.xticks(x_chain, bjointsp_chain_length)
+    plt.plot(x_chain, emu_chain_rtt, 'g^', x_chain, sim_chain_rtt, 'bs')
+    # plt.xticks(x_chain, sim_chain_length)
     plt.legend(['Emulation', 'Placement (simulation)'])
     plt.ylabel('Chain RTT (in ms)')
     plt.title('Delay comparison between emulation and simulation')
@@ -68,26 +77,26 @@ def plot():
     # plot inter-VNF delays
     x_vnfs = range(len(emu_vnf_rtt))
     plt.subplot(2, 1, 2)
-    plt.plot(x_vnfs, emu_vnf_rtt, 'g^', x_vnfs, bjointsp_vnf_rtt, 'bs')
-    plt.errorbar(x_vnfs, emu_vnf_rtt, yerr=emu_vnf_std, fmt='none', ecolor='black', capsize=2)
-    plt.xticks(x_vnfs, bjointsp_vnf_length)
+    plt.plot(x_vnfs, emu_vnf_rtt, 'g^', x_vnfs, sim_vnf_rtt, 'bs')
+    # plt.errorbar(x_vnfs, emu_vnf_rtt, yerr=emu_vnf_std, fmt='none', ecolor='black', capsize=2)
+    # plt.xticks(x_vnfs, sim_vnf_length)
     plt.xlabel('Different service/source combinations (chain length)')
     plt.ylabel('Inter-VNF RTT (in ms)')
 
 
     # plot delay differences
     plt.figure(2)
-    chain_diffs = [emu_chain_rtt[i]-bjointsp_chain_rtt[i] for i in range(len(emu_chain_rtt))]
+    chain_diffs = [emu_chain_rtt[i]-sim_chain_rtt[i] for i in range(len(emu_chain_rtt))]
     plt.subplot(2, 1, 1)
     plt.plot(x_chain, chain_diffs)
-    plt.xticks(x_chain, bjointsp_chain_length)
+    # plt.xticks(x_chain, sim_chain_length)
     plt.ylabel('Chain RTT diff. (in ms)')
     plt.title('Emulation delay - simulation delay')
 
-    vnf_diffs = [emu_vnf_rtt[i]-bjointsp_vnf_rtt[i] for i in range(len(emu_vnf_rtt))]
+    vnf_diffs = [emu_vnf_rtt[i]-sim_vnf_rtt[i] for i in range(len(emu_vnf_rtt))]
     plt.subplot(2, 1, 2)
     plt.plot(x_vnfs, vnf_diffs)
-    # plt.xticks(x_vnfs, bjointsp_vnf_length)
+    # plt.xticks(x_vnfs, sim_vnf_length)
     plt.xlabel('Different service/source combinations (chain length)')
     plt.ylabel('Inter-VNF RTT diff. (in ms)')
 
@@ -100,5 +109,6 @@ def plot():
 
 
 if __name__ == '__main__':
-    pass
-    # TODO: call other methods: process, then plot
+    emu_delays = process_emu_results()
+    sim_delays = process_sim_results()
+    plot(emu_delays, sim_delays)
