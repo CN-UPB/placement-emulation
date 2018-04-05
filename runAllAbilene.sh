f99ac7a080d3b700457f17710e52e99afe8e1316
Stefan Schneider <stefan.schneider@upb.de>
2018-04-05 10:08:13 +0200

    added script for eval on Airtel, updated script for Abilene and Colt (use all 10 sources)

--- runAllAbilene.sh
+++ runAllAbilene.sh
@@ -7,6 +7,11 @@ set -e
 ./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source2.yaml -c 100
 ./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source3.yaml -c 100
 ./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source4.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source5.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source6.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source7.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source8.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source9.yaml -c 100
 
 # fw2chain
 ./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source0.yaml -c 100
@@ -14,6 +19,11 @@ set -e
 ./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source2.yaml -c 100
 ./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source3.yaml -c 100
 ./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source4.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source5.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source6.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source7.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source8.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source9.yaml -c 100
 
 # fw3chain
 ./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source0.yaml -c 100
@@ -21,4 +31,8 @@ set -e
 ./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source2.yaml -c 100
 ./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source3.yaml -c 100
 ./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source4.yaml -c 100
-
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source5.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source6.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source7.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source8.yaml -c 100
+./place-emu.sh -n inputs/networks/Abilene.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source9.yaml -c 100
---
+++ runAllAirtel.sh
@@ -0,0 +1,38 @@
+# sequentially run multiple (all?) combinations of service-source on Airtel
+set -e
+
+# fw1chain
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source0.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source1.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source2.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source3.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source4.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source5.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source6.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source7.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source8.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw1chain.yaml -s inputs/sources/source9.yaml -c 100
+
+# fw2chain
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source0.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source1.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source2.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source3.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source4.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source5.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source6.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source7.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source8.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw2chain.yaml -s inputs/sources/source9.yaml -c 100
+
+# fw3chain
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source0.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source1.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source2.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source3.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source4.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source5.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source6.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source7.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source8.yaml -c 100
+./place-emu.sh -n inputs/networks/Airtel.graphml -t inputs/services/fw3chain.yaml -s inputs/sources/source9.yaml -c 100
